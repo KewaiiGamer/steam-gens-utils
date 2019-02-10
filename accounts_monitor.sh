97addc2ea34891d93f7453dda04a37f5d864298b
KewaiiGameri <miguelviegas1999@gmail.com>
2019-02-10 23:04:31 +0100

    Updated accounts_monitor.sh

--- accounts_monitor.sh
+++ accounts_monitor.sh
@@ -2,13 +2,25 @@
 echo "Script made by Kewaii#3167"
 oldQty=0
 qty=($(wc -l accounts.txt))
-echo $qty accounts found
+oldQty=qty
+echo $(date -u +%T) $qty accounts found
 sleep 60;
+everysec=false
 while true; do
   newQty=($(wc -l accounts.txt))
   difference=$((newQty - oldQty))
   oldQty=$newQty
-  echo Generated $difference accounts since last minute
-  sleep 60;
+  if [ "$difference" = 0 ]; then
+     echo $(date -u +%T) No new accounts were generated. Recheck interval was set to 1 second
+     everysec=true
+  else
+     echo $(date -u +%T) Generated $difference accounts since last minute
+     everysec=false
+  fi
+  if [ "$everysec" = true ]; then
+     sleep 1;
+  else
+     sleep 60;
+  fi
 done
 
